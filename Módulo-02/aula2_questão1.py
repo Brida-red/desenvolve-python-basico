4bb6c846dd434e95c59c1f208941c0f692a68584
BrunaSoares-ProjetoDesenvolve <bruna.gontijo@pdbomdespacho.com.br>
2025-04-23 13:07:06 -0300

    Add files via upload

---
+++ Módulo-02/aula2_questão1.py
@@ -0,0 +1,7 @@
+num1 = 5
+num2 = 2
+print (type (num1))
+print (type (num2))
+num3 = num1/num2
+print (num3)
+print (type (num3))
---
+++ Módulo-02/aula2_questão2.py
@@ -0,0 +1,12 @@
+variável_1 = "o resultado é"
+variável_2 = 10
+variável_3 = 3.5
+variável_4 = variável_2 + variável_3
+print (variável_1)
+print (type (variável_1))
+print (variável_2)
+print (type (variável_2))
+print (variável_3)
+print (type (variável_3))
+print (variável_4)
+print (type (variável_4))
