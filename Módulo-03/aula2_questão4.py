46400a76c3dcbbd0b28790108631d215bb9ed38a
BrunaSoares-ProjetoDesenvolve <bruna.gontijo@pdbomdespacho.com.br>
2025-04-29 11:26:15 -0300

    Add files via upload

---
+++ Módulo-03/aula2_questão4.py
@@ -0,0 +1,10 @@
+classe = input ("Escolha a classe (mago, guerreiro ou arqueiro)") 
+pontos_força = int(input ("Digite os pontos de força"))
+pontos_magia = int(input ("Digite os pontos de magia"))
+
+mago = classe == 'mago'; pontos_força <= 10 and pontos_magia >= 15
+guerreiro = classe == 'guerreiro'; pontos_força >= 15 and pontos_magia <= 10
+arqueiro = classe == 'arqueiro'; (pontos_força > 5) and (pontos_magia > 5) and (pontos_força <= 15) and (pontos_magia <= 15)
+pontos_atribuidos = mago or guerreiro or arqueiro 
+
+print = input(f"Pontos de atributo consistentes com a classe escolhida:{pontos_atribuidos}")
